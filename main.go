1a4b7a52e61db29942be6e92d1375f6530b7ab8f
RJ <zthvivid@163.com>
2022-04-15 00:37:34 +0800

    使用模板文件

--- main.go
+++ main.go
@@ -112,29 +112,6 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
 	} else {
 		// fmt.Fprintf(w, "有错误发生，errors 的值为: %v <br>", errors)
-		html := `
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<title>创建文章 —— 我的技术博客</title>
-				<style type="text/css">.error {color: red;}</style>
-			</head>
-			<body>
-				<form action="{{ .URL }}" method="post">
-					<p><input type="text" name="title" value="{{ .Title }}"></p>
-					{{ with .Errors.title }}
-					<p class="error">{{ . }}</p>
-					{{ end }}
-					<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
-					{{ with .Errors.body }}
-					<p class="error">{{ . }}</p>
-					{{ end }}
-					<p><button type="submit">提交</button></p>
-				</form>
-			</body>
-			</html>
-			`
-
 		storeURL, _ := router.Get("articles.store").URL()
 
 		data := ArticlesFormData{
@@ -143,7 +120,7 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 			URL:    storeURL,
 			Errors: errors,
 		}
-		tmpl, err := template.New("create-form").Parse(html)
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
@@ -183,24 +160,22 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "创建博文表单")
-	html := `
-		<!DOCTYPE html>
-		<html lang="en">
-			<head>
-					<title>创建文章 —— 我的技术博客</title>
-			</head>
-			<body>
-				<form action="%s?test=data" method="post">
-					<p><input type="text" name="title"></p>
-					<p><textarea name="body" cols="30" rows="10"></textarea></p>
-					<p><button type="submit">提交</button></p>
-				</form>
-			</body>
-		</html>
-	`
-
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
