834732fa47eef861668084ca7ba938b3c885b99e
RJ <zthvivid@163.com>
2022-04-22 00:14:03 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/zhangtaohua/goblog/app/models/user"
+	"github.com/zhangtaohua/goblog/app/requests"
 	"github.com/zhangtaohua/goblog/pkg/view"
 )
 
@@ -20,26 +21,36 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 
-	// 0. 初始化变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	// 1. 表单验证
-	// 2. 验证通过 —— 入库，并跳转到首页
+	// 1. 初始化数据
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	_user.Create()
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 有错误发生，打印数据
+		// data, _ := json.MarshalIndent(errs, "", "  ")
+		// fmt.Fprint(w, string(data))
+
+		// 3. 表单不通过 —— 重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
-	}
+		// 4. 验证成功，创建数据
+		_user.Create()
 
-	// 3. 表单不通过 —— 重新显示表单
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+	}
 }
