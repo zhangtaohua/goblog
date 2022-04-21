b1c3b60ebef7b57c3606a053745158972fedcd25
RJ <zthvivid@163.com>
2022-04-21 18:10:12 +0800

    注册页面

---
+++ app/http/controllers/auth_controller.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/zhangtaohua/goblog/pkg/view"
+)
+
+// AuthController 处理静态页面
+type AuthController struct {
+}
+
+// Register 注册页面
+func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
+	view.Render(w, view.D{}, "auth.register")
+}
+
+// DoRegister 处理注册逻辑
+func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
+	//
+}
