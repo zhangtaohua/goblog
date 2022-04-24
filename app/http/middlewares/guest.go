7bcd6518b7e959f91b3aa2a026368a916134ce8f
RJ <zthvivid@163.com>
2022-04-24 15:37:36 +0800

    授权中间件

---
+++ app/http/middlewares/guest.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/zhangtaohua/goblog/pkg/auth"
+	"github.com/zhangtaohua/goblog/pkg/flash"
+)
+
+// Guest 只允许未登录用户访问
+func Guest(next HttpHandlerFunc) HttpHandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if auth.Check() {
+			flash.Warning("登录用户无法访问此页面")
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+
+		// 继续处理接下去的请求
+		next(w, r)
+	}
+}
